08041bc54b9fc6e8c43c405856e190076559791d
Adam Bedard <adam.bedard@icloud.com>
2019-11-18 16:44:37 -0500

    Added baby controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'babies/index'
+  get 'babies/show'
+  get 'babies/new'
+  get 'babies/create'
+  get 'babies/update'
+  get 'babies/edit'
+  get 'babies/destroy'
   devise_for :users
   root to: 'pages#home'
   resources :users
