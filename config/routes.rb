d63e830db702081daa987c4ec6afddc52d4113fc
Athena Vassilopoulos <athena.vassilo@gmail.com>
2019-11-20 15:12:32 -0500

    in updating user details, updated registration controller, devise etc

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'registrations' }
   root to: 'babies#index'
 
   resources :babies, only: [:create, :new, :show, :destroy, :update, :edit] do
@@ -8,6 +8,6 @@ Rails.application.routes.draw do
 
   resources :bookings, only: :destroy
 
-  resources :users, only: [:show, :edit, :update]
+  resources :users, only: [:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
