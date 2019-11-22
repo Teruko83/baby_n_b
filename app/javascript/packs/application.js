7fed741d5aeab10f8cc31587118149f894957405
aurelie mathys <mathyau@gmail.com>
2019-11-22 15:19:09 -0500

    coorection navbar, icone, form add baby

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,7 @@
 
 console.log('Hello World from Webpacker')
 
+import 'bootstrap'
 import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
 
 import { initMapbox } from '../plugins/init_mapbox';
