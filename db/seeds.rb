06f5efa18f3042b142e176612ae8168fb6524075
aurelie mathys <mathyau@gmail.com>
2019-11-22 11:09:49 -0500

    proprio photo vignette

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,9 +12,9 @@
   user2 = User.create!(name: "Oraly", email: "Oraly@email.com", password: "123456", remote_photo_url: "https://source.unsplash.com/random?man?#{rand}")
   user3 = User.create!(name: "Anthena", email: "Anthena@email.com", password: "123456", remote_photo_url: "https://source.unsplash.com/random?woman?#{rand}")
 
-  Baby.create!(first_name: "Johnny", date_of_birth: "01/02/2010", category: "calm", bio:"Nice baby super friendly", price: "3", user: user1, remote_photo_url: "https://source.unsplash.com/random?baby?#{rand}")
-  Baby.create!(first_name: "Philippe", date_of_birth: "10/03/2008", category: "calm", bio:"Nice baby super friendly", price: "15", user: user1, remote_photo_url: "https://source.unsplash.com/random?baby?#{rand}")
-  Baby.create!(first_name: "Stephane", date_of_birth: "26/02/2015", category: "good spirit", bio:"Nice baby super friendly", price: "150", user: user1, remote_photo_url: "https://source.unsplash.com/random?baby?#{rand}")
+  Baby.create!(first_name: "Katie", date_of_birth: "01/02/2010", category: "calm", bio:"Nice baby super friendly", price: "30", user: user1, remote_photo_url: "https://source.unsplash.com/random?baby?#{rand}")
+  Baby.create!(first_name: "Martin", date_of_birth: "10/03/2008", category: "geek", bio:"Nice baby super friendly", price: "15", user: user1, remote_photo_url: "https://source.unsplash.com/random?baby?#{rand}")
+  Baby.create!(first_name: "Stephane", date_of_birth: "26/02/2015", category: "Jedi Master", bio:"Nice baby super friendly", price: "150", user: user1, remote_photo_url: "https://source.unsplash.com/random?baby?#{rand}")
   Baby.create!(first_name: "MG", date_of_birth: "01/05/2018", category: "calm", bio:"Nice baby super friendly", price: "50", user: user2, remote_photo_url: "https://source.unsplash.com/random?baby?#{rand}")
   Baby.create!(first_name: "Simon", date_of_birth: "01/02/2010", category: "calm", bio:"Nice baby super friendly", price: "15", user: user3, remote_photo_url: "https://source.unsplash.com/random?baby?#{rand}")
   Baby.create!(first_name: "Xavier", date_of_birth: "01/02/2010", category: "calm", bio:"Nice baby super friendly", price: "15", user: user1, remote_photo_url: "https://source.unsplash.com/random?baby?#{rand}")
